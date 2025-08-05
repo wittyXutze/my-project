9469688e3b91430cfc21e2d155e13b5f0c2ca301
withold <withold.schuetze@gmx.de>
2025-08-05 23:52:50 +0200

    fix: remove npm plugin to avoid missing token 1

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,18 @@
 module.exports = {
-  branches: ["main", { name: "rc", prerelease: true }],
+  branches: [
+    "main",
+    { name: "rc", prerelease: true }
+  ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/git",
+    ["@semantic-release/changelog", {
+      changelogFile: "CHANGELOG.md"
+    }],
+    ["@semantic-release/git", {
+      assets: ["CHANGELOG.md", "src/version.h"],
+      message: "chore(release): ${nextRelease.version} [skip ci]"
+    }],
     "@semantic-release/github"
   ]
 };
